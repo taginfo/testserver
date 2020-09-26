030726f21f1a18f41dc86f8afa2807afcaecdc7b
Jochen Topf <jochen@topf.org>
2020-09-26 16:35:11 +0200

    Don't download taginfo-search.db, it doesn't exist any more

--- download-databases.sh
+++ download-databases.sh
@@ -8,7 +8,7 @@ set -x
 
 DIR=/srv/taginfo/data
 
-for db in master history db wiki languages projects search; do
+for db in master history db wiki languages projects; do
     file="taginfo-${db}.db.bz2"
     curl --silent --location --output $DIR/$file https://taginfo.openstreetmap.org/download/$file
     bunzip2 $DIR/$file
