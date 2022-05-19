e681ad65a45f63f630253563771642cb4bb05f4f
Jochen Topf <jochen@topf.org>
2022-05-19 15:28:04 +0200

    Also download chronology database in download script

--- download-databases.sh
+++ download-databases.sh
@@ -8,7 +8,7 @@ set -x
 
 DIR=/srv/taginfo/data
 
-for db in master history db wiki languages projects; do
+for db in master history db wiki languages projects chronology; do
     file="taginfo-${db}.db.bz2"
     curl --silent --location --output $DIR/$file https://taginfo.openstreetmap.org/download/$file
     bunzip2 $DIR/$file
